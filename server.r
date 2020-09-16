ee5271587bd04f6579e6063a3c5ae0d8b1623d5b
cmjt <cmjt@st-andrews.ac.uk>
2020-09-16 20:28:25 +1200

    creating hidden experiment pages and styling back buttons

--- server.r
+++ server.r
@@ -1,7 +1,48 @@
 ## server.R ##
 function(input, output,session) {
-    output$distPlot <- renderPlot({
-        hist(rnorm(input$obs), col = 'darkgray', border = 'white')
+    ## go back to landing page
+    observe({
+        onclick("back_to_landing_tom", {
+            shinyjs::show("landing_page")
+            shinyjs::hide("experiment_page_tomato")
+            shinyjs::removeClass(selector = "body", class = "experiment_page_tomato")
+        })
+    })
+    observe({
+        onclick("back_to_landing_chick", {
+            shinyjs::show("landing_page")
+            shinyjs::hide("experiment_page_chick")
+            shinyjs::removeClass(selector = "body", class = "experiment_page_chick")
+        })
+    })
+    observe({
+        onclick("back_to_landing_pump", {
+            shinyjs::show("landing_page")
+            shinyjs::hide("experiment_page_pump")
+            shinyjs::removeClass(selector = "body", class = "experiment_page_pump")
+        })
+    })
+    ## load experiment pages
+    observe({
+        onclick("tomato_butt", {
+            shinyjs::hide("landing_page")
+            shinyjs::show("experiment_page_tomato")
+            shinyjs::removeClass(selector = "body", class = "landing_page")
+        })
+    })
+    observe({
+        onclick("chick_butt", {
+            shinyjs::hide("landing_page")
+            shinyjs::show("experiment_page_chick")
+            shinyjs::removeClass(selector = "body", class = "landing_page")
+        })
+    })
+    observe({
+        onclick("pump_butt", {
+            shinyjs::hide("landing_page")
+            shinyjs::show("experiment_page_pump")
+            shinyjs::removeClass(selector = "body", class = "landing_page")
+        })
     })
     session$onSessionEnded(stopApp)
-}
+    }
--- ui.r
+++ ui.r
@@ -2,16 +2,54 @@
 ## Virtual Experiments to Teach Experimental Design
 ## remotes::install_github("JohnCoene/typed")
 library(typed)
+library(shiny)
+library(shinyjs)
 fluidPage(
+    ## set up shinyjs
+    useShinyjs(),
     title = "VEsTED",
     ## CSS style file
     includeCSS("www/style.css"),
-    ## title
-    h1("Choose your virtual experiment"),
-    ## R package for typed.js
-    typed(list(h2("randomisation"),h2("blocking"),h2("replication")), typeSpeed = 100, loop = TRUE),
-    ## Buttons for each experiment
-    div(id = "egg", class = "mid",actionButton("",img(src = "img/chick.png"),class = "egg_btn")),
-    div(id = "tomato", class = "mid",actionButton("",img(src = "img/tomato.png"),class = "tomato_btn")),
-    div(id = "pimpkin", class = "mid",actionButton("",img(src = "img/pumpkin.png"),class = "pumpkin_btn"))
-)
+    div(id = "landing_page",
+        ## title
+        h1("Choose your virtual experiment"),
+        ## R package for typed.js
+        typed::typed(list(h2("randomisation"),h2("blocking"),h2("replication")), typeSpeed = 100, loop = TRUE),
+        ## Buttons for each experiment
+        div(id = "tomato", class = "mid",
+            actionButton("tomato_butt",div(class = "container",img(src = "img/tomato.png"),
+                                           div(class = "overlay",
+                                               h3("Is horse manure"), h3("the age-old miracle"), h3("grow?"))),
+                         class = "tomato_btn")),
+        div(id = "egg", class = "mid",
+            actionButton("chick_butt",div(class = "container",img(src = "img/chick.png"),
+                                          div(class = "overlay",
+                                              h3("Chooks favourite foods"))),
+                         class = "egg_btn")),
+        div(id = "pumpkin", class = "mid",
+            actionButton("pump_butt", div(class = "container",img(src = "img/pumpkin.png"),
+                                          div(class = "overlay",
+                                              h3("Esther and Gwenllian"), h3("want to know who"),
+                                              h3("reigns supreme"), h3("pumpkin growing"))),
+                         class = "pumpkin_btn"))
+        ),
+    ## Hidden experiment pages
+    ## Tomato
+    shinyjs::hidden(div(id = "experiment_page_tomato",
+               div(id = "back_to_landing_tom", 
+                   tags$a(class="btn btn-primary", icon("arrow-left"), h4("Back to landing page")),
+                   h1("Tomato experiment")
+                   ))),
+    ## Chicken
+    shinyjs::hidden(div(id = "experiment_page_chick",
+               div(id = "back_to_landing_chick", 
+                   tags$a(class="btn btn-primary", icon("arrow-left"), h4("Back to landing page")),
+                   h1("Chicken experiment")
+                   ))),
+    ## Pumpkin
+    shinyjs::hidden(div(id = "experiment_page_pump",
+               div(id = "back_to_landing_pump", 
+                   tags$a(class="btn btn-primary", icon("arrow-left"), h4("Back to landing page")),
+                   h1("Pumpkin experiment")
+                   ))),
+    )

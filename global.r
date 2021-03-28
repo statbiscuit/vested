3d0c4c3b71882e5b11f7ab6fbc05ee97fab8d614
cmjt <cmjonestodd@gmail.com>
2021-03-29 12:53:45 +1300

    auto package install

--- global.r
+++ global.r
@@ -1,16 +1,18 @@
-## remotes::install_github("JohnCoene/typed")
-## remotes::install_github("ayayron/shinydnd")
-library(typed)
-library(shiny)
-library(shinyjs)
-library(shinyWidgets)
-library(shinyRadioMatrix)
-library(ggplot2)
-library(ggsn)
-library(shinyDND)
-library(stringr)
+## CRAN packages
+pkgs <- c("shinyjs","shinyWidgets","shinyRadioMatrix","ggplot2","ggsn","shinyDND","stringr")
+options(warn = -1)
+for (i in pkgs){
+  if (!require(i, quietly = TRUE, character.only = TRUE)){
+    install.packages(i)
+  }
+  require(i, quietly = TRUE, character.only = TRUE)
+}
+## packages from GitHub
+if(!require("typed",quietly = TRUE, character.only = TRUE)) remotes::install_github("JohnCoene/typed")
+require(typed)
+options(warn = 0)
 
-
+## experiment scripts
 source("scripts/pumpkin.r")
 source("scripts/chickens.r")
 source("scripts/tomgro.r")
